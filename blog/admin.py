f9ac932a621e7555f6e8b4baecdcc94ebc92badb
dagmara-szproch <dagmamara@gmail.com>
2025-08-17 16:01:19 +0100

    customise the admin page

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content',)
-    list_display = ('title', 'slug', 'status')
-    list_filter = ('status',)
-    search_fields = ['title']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
 admin.site.register(Comment)
