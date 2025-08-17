a46ed401afa11737e43e2c82c17e350f69591d8c
dagmara-szproch <dagmamara@gmail.com>
2025-08-17 13:14:49 +0100

    modify the standard admin dashboard using djangoo methods and summernote package

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status')
+    list_filter = ('status',)
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
